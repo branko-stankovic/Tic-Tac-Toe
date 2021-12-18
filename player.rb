01533543d0e38a2c2b48e3e8202954fd9b154b5c
Branko Stankovic <branko.stankovic@gmail.com>
2021-12-18 23:27:36 +0100

    Clear terminal when starting new game. Print the rules of the game

--- player.rb
+++ player.rb
@@ -1,7 +1,13 @@
 # module for interacting with the player
 module Player
   def welcome
+    puts `clear`
     puts 'Welcome to a game of Tic-Tac-Toe!'
+    puts
+    puts 'Player 1 plays as: X'
+    puts 'Player 2 plays as: Y'
+    puts
+    puts 'The first player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner!'
   end
 
   def pick_side
